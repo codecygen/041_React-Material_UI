a72931eeb5096940cb9d063b24feb67d8fd9c075
gingercook <vahitaras@gmail.com>
2021-12-08 23:23:20 -0500

    react material ui complete tutorial

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SideMenu from '../components/SideMenu';
+
+function App() {
+  return (
+    <>
+      <SideMenu />
+      <div>Hi</div>
+    </>
+  );
+}
+
+export default App;
