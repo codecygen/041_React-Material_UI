ce9955a24f66a7c8fd5566e6dc96075a95821a8e
gingercook <vahitaras@gmail.com>
2021-12-08 22:44:16 -0500

    new react project

--- src/App.js
+++ src/App.js
@@ -1,68 +1,25 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import AppBar from '@mui/material/AppBar';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Box from "@mui/material/Box";
-import Menu from '@mui/material/Menu';
-import './index.css';
-import { createTheme } from '@mui/material/styles';
-
-const pages = ["Home", "About", "Projects", "Contact"];
-
-const theme = createTheme({
-    palette: {
-        primary: {
-            light: '#757ce8',
-            main: '#424b9b',
-            dark: '#002884',
-            contrastText: '#fff',
-        },
-        secondary: {
-            light: '#ff7961',
-            main: '#f44336',
-            dark: '#ba000d',
-            contrastText: '#000',
-        },
-    },
-});
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
-    return (
-        <>
-
-            <AppBar position="sticky" theme={theme}>
-                <Container maxWidth="xl">
-                    <Toolbar disableGutters>
-                        <Typography
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ display: { xs: 'none', md: 'flex' } }}
-                        >
-                            Aras Sen
-                        </Typography>
-                        <Box
-                            variant="h6"
-                            noWrap
-                            component="div"
-                            sx={{ display: { xs: 'none', md: 'flex' } }}
-                        >
-                            {pages.map(page => (
-                                <Button>
-                                    Aras
-                                </Button>
-                            ))}
-                        </Box>
-                        <Menu></Menu>
-                    </Toolbar>
-                </Container>
-            </AppBar>
-            <Button variant="contained">Hello World</Button>
-        </>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
